1daaed5aba07d8b22f7acfe48eba7dd4ad4ef211
Eugen Guriev <eugeniy.guriev@kralys.ch>
2022-04-06 14:21:47 +0300

    initial commit to a new branch

--- client/src/Pdfgen.js
+++ client/src/Pdfgen.js
@@ -1,55 +1,114 @@
-import React, { useState } from 'react'
-import { Editor } from '@tinymce/tinymce-react';
-
+import React, { useEffect, useState } from "react";
+import { Editor } from "@tinymce/tinymce-react";
+import axios from 'axios';
 
 const Pdfgen = () => {
-    const [data, setData] = useState('')
-    const [items, setItems] = useState([])
-    const [name, setName] = useState('')
-    const addTemplate = () => {
-        setItems(prev => [...prev, {
-            name,
-            tamplete: data
-        }])
-        setName('')
-        setData('')
-    }
-    const addToData = (string) => {
-        setData(`${data} ${string}`)
-    }
-  return (
-    <div style={{display: 'flex', justifyContent: 'space-around'}}>
-    <div style={{width: '550px', background: '#fff', padding: '10px', boxSizing: 'border-box', borderRadius: '10px'}}>
-    <Editor
-         onEditorChange={e => {setData(e)
-         console.log(e)
-         }}
-         value={data}
-         init={{
-           height: 500,
-           menubar: false,
-           plugins: 'table code advtable lists fullscreen hr',
-            toolbar: 'undo redo | formatselect | bold italic hr | ' +
-                'alignleft aligncenter alignright alignjustify | indent outdent | ' +
-                'table tableinsertdialog tablecellprops tableprops advtablerownumbering | fullscreen',
-  
-           content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px;  } p {margin: 0}'
-         }}
-       />
-        <input type={'text'} placeholder='name of tamplete' onChange={e => setName(e.target.value)}/>
-       <button className='btn' onClick={addTemplate}>Add Template</button>
-    </div>
-        <div>
-        <div style={{width: '475px', height: '500px', background: '#fff', padding: '10px', boxSizing: 'border-box',}}>
-            <div style={{width: '475px'}} className='special' dangerouslySetInnerHTML={{__html: data}}/>
-            
-            </div>
-            <div className='tapletes'>
-                {items.map((item) => <div className='item' onClick={() => addToData(item.tamplete)}>{item.name}</div>)}
-            </div>
-        </div>
-    </div>
-  )
-}
 
-export default Pdfgen
+  const host = 'http://localhost:5000'
+
+  const style = '<style> @import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"); *{font-family: "Roboto", sans-serif;} p {margin: 0; font-weight:normal; font-size: 13px} strong {font-size:15px; font-weight: 600; font-weight:bold;} tr, td, table, tbody {border: none} div.wrapper {padding-top: 20px; padding-left: 40px; padding-right: 30px; }  </style> '
+
+  const [data, setData] = useState("");
+  const [items, setItems] = useState([]);
+  const [name, setName] = useState("");
+
+
+  useEffect(() => {
+    console.log(data)
+  }, [data])
+
+  const addTemplate = () => {
+    setItems((prev) => [
+      ...prev,
+      {
+        name,
+        tamplete: data,
+      },
+    ]);
+    setName("");
+    setData("");
+  };
+
+  const generatePdf = () => {
+    console.log(data);
+    axios.post(`${host}/generate-pdf`, {data})
+    .then(res => {
+        console.log(res.data)
+    })
+  }
+
+  const addToData = (string) => {
+    setData(`${data} ${string}`);
+  };
+  return (
+    <div style={{ display: "flex", justifyContent: 'space-around'}}>
+      <div
+        style={{
+          width: '635px',
+          background: "#fff",
+          padding: "10px",
+          boxSizing: "border-box",
+          borderRadius: "10px",
+        }}
+      >
+        <Editor
+          onEditorChange={(e) => {
+            setData(`${style} ${e}`);
+          }}
+          value={data}
+          init={{
+            height: 842,
+            menubar: false,
+            plugins: "table code advtable lists fullscreen hr",
+            toolbar:
+              "undo redo | formatselect | bold italic hr | " +
+              "alignleft aligncenter alignright alignjustify | indent outdent | " +
+              "table tableinsertdialog tablecellprops tableprops advtablerownumbering | fullscreen",
+
+            content_style:
+              "body { font-family:Helvetica,Arial,sans-serif; font-size:14px; margin: 10px } p {margin: 0}",
+          }}
+        />
+        <input
+          type={"text"}
+          placeholder="name of tamplete"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <button className="btn" onClick={addTemplate}>
+          Add Template
+        </button>
+
+        <button className="btn" onClick={generatePdf}>generate pdf</button>
+      </div>
+
+
+
+
+      <div>
+        <div
+          style={{
+            height: "862px",
+            background: "#fff",
+            padding: "10px",
+            boxSizing: "border-box",
+          }}
+        >
+          <div
+            style={{ width: "595px" }}
+            className="special"
+            dangerouslySetInnerHTML={{ __html: data }}
+          />
+        </div>
+        <div className="tapletes">
+          {items.map((item) => (
+            <div className="item" onClick={() => addToData(item.tamplete)}>
+              {item.name}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Pdfgen;
--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const cors = require('cors');
 var app = express();
 var pdf = require('html-pdf');
 var options = { format: 'A4' };
@@ -6,18 +7,30 @@ var fs = require('fs');
 var html_to_pdf = require('html-pdf-node');
 
 
+app.use(cors());
+app.use(express.json()); 
 app.listen(5000, () => {
     console.log('server work');
 })
 
+app.use(express.static('public'));
+
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/', function(req, res) {
-    const style = '<style> @import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"); *{font-family: "Roboto", sans-serif;} p {margin: 0; font-weight:normal; font-size: 13px} strong {font-size:15px; font-weight: 600; font-weight:bold;} div { padding-top: 20px; padding-left: 30px; }</style>'
-    const string = '<div><p><strong>Dr. Gilles TARDIEU</strong></p><p>M&eacute;decine g&eacute;n&eacute;rale</p><p>N&deg;RCC : N284622</p><p>&nbsp;</p><p><strong>Cabinet M&eacute;dical Villeneuve Montreux,&nbsp;</strong>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;the 05.04.2022</p><p>Rue des Fortifications 8ba</p><p>1821 Montreux</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p><strong>Concerns: Cher Dr. Tardieu,</strong></p><hr /><p>&nbsp;</p></div>'
-    const ress = `${style} ${string}`
-    pdf.create(ress, options).toFile('./businesscard7.pdf', function(err, res) {
+app.post('/generate-pdf', (req, res) => {
+
+    const {data} = req.body;
+
+    console.log(data)
+
+
+
+    // const style = '<style> @import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"); *{font-family: "Roboto", sans-serif;} p {margin: 0; font-weight:normal; font-size: 13px} strong {font-size:15px; font-weight: 600; font-weight:bold;} div { padding-top: 20px; padding-left: 30px; } tr, td, table, tbody {border: none !important} </style>'
+    const string = data;
+    const ress = `<div class="wrapper">${string}</div>`
+    pdf.create(ress, options).toFile('./public/businesscard7.pdf', function(err, res) {
         if (err) return console.log(err);
         console.log(res); // { filename: '/app/businesscard.pdf' }
-      });
+    });
+    res.send('resolved');
 });
 
